93c330ce16af5bd8072f91808a3f3a9bfc97e741
kunish <kunish.butt@gmail.com>
2023-10-08 00:43:22 +0800

    feat(i18n): refresh

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,16 +1,19 @@
 'use client'
 
 import { RefreshCwIcon } from 'lucide-react'
+import { useTranslation } from 'react-i18next'
 import { Button } from '~/components/ui/button'
 
 export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+  const { t } = useTranslation()
+
   return (
     <div className="container flex h-screen flex-col items-center justify-center gap-4">
       <div className="text-center font-bold">{error.message}</div>
 
       <Button className="gap-2" onClick={() => reset()}>
         <RefreshCwIcon className="w-4" />
-        Refresh
+        {t('actions.refresh')}
       </Button>
     </div>
   )
