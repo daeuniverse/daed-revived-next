2457049ffe52862e0562f50ee0c24434aeffdeb4
kunish <kunish.butt@gmail.com>
2023-10-19 23:05:06 +0800

    feat(orchestrate): selects instead of tables

--- src/app/(protected)/orchestrate/page.tsx
+++ src/app/(protected)/orchestrate/page.tsx
@@ -1,24 +1,9 @@
 'use client'
 
-import {
-  Accordion,
-  AccordionItem,
-  Listbox,
-  ListboxItem,
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-  Table,
-  TableBody,
-  TableCell,
-  TableColumn,
-  TableHeader,
-  TableRow,
-  useDisclosure
-} from '@nextui-org/react'
+import { Accordion, AccordionItem, Chip, Select, SelectItem } from '@nextui-org/react'
 import { IconPlus, IconRefresh, IconTrash } from '@tabler/icons-react'
 import dayjs from 'dayjs'
-import { FC, Key, ReactNode, useCallback, useMemo, useState } from 'react'
+import { FC, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import {
   useGroupAddNodesMutation,
@@ -66,6 +51,14 @@ const GroupTable: FC<{
   subscriptions: {
     id: string
     tag?: string | null
+
+    nodes: {
+      edges: {
+        id: string
+        name: string
+        tag?: string | null
+      }[]
+    }
   }[]
 
   nodes: {
@@ -79,342 +72,108 @@ const GroupTable: FC<{
   const { t } = useTranslation()
 
   // subscriptions
-  const {
-    isOpen: isAddSubscriptionsTooltipOpen,
-    onOpenChange: onAddSubscriptionsTooltipOpenChange,
-    onOpen: onAddSubscriptionsTooltipOpen,
-    onClose: onAddSubscriptionsTooltipClose
-  } = useDisclosure()
-
+  const allSubscriptions = useMemo(
+    () => group.subscriptions.map((subscription) => subscription.id),
+    [group.subscriptions]
+  )
   const selectableSubscriptions = useMemo(() => {
     const selectedSubscriptionIDs = group.subscriptions.map((subscription) => subscription.id)
 
     return subscriptions.filter((subscription) => !selectedSubscriptionIDs.includes(subscription.id))
   }, [group.subscriptions, subscriptions])
-  const [selectedSubscriptionsToRemove, setSelectedSubscriptionsToRemove] = useState<string[]>([])
-  const allSubscriptions = useMemo(
-    () => group.subscriptions.map((subscription) => subscription.id),
-    [group.subscriptions]
-  )
-  const [selectedSubscriptionsToAdd, setSelectedSubscriptionsToAdd] = useState<string[]>([])
   const groupAddSubscriptionsMutation = useGroupAddSubscriptionsMutation()
   const groupDelSubscriptionsMutation = useGroupDelSubscriptionsMutation()
 
-  const subscriptionTableColumns: {
-    key: string
-    name: string
-  }[] = [
-    { key: 'tag', name: t('primitives.tag') },
-    {
-      key: 'updatedAt',
-      name: t('primitives.updatedAt')
-    },
-    { key: 'node', name: t('primitives.node') },
-    { key: 'remove', name: t('actions.remove') }
-  ]
-
-  const renderSubscriptionCell = useCallback(
-    (subscription: Group['subscriptions'][number], columnKey: Key) => {
-      const cellValue = subscription[columnKey as keyof Group['subscriptions'][number]] as ReactNode
-
-      switch (columnKey) {
-        case 'updatedAt':
-          return dayjs(cellValue as string).format('YYYY-MM-DD hh:mm:ss')
-
-        case 'node':
-          return subscription.nodes.edges.length
-
-        case 'remove':
-          return (
-            <Button
-              color="danger"
-              isIconOnly
-              onPress={async () => {
-                await groupDelSubscriptionsMutation.mutateAsync({
-                  id: group.id,
-                  subscriptionIDs: [subscription.id]
-                })
-                await refetch()
-              }}
-            >
-              <IconTrash />
-            </Button>
-          )
-
-        default:
-          return cellValue
-      }
-    },
-    [group.id, groupDelSubscriptionsMutation, refetch]
-  )
-
   // nodes
-  const {
-    isOpen: isAddNodesTooltipOpen,
-    onOpenChange: onAddNodesTooltipOpenChange,
-    onOpen: onAddNodesTooltipOpen,
-    onClose: onAddNodesTooltipClose
-  } = useDisclosure()
+  const allNodes = useMemo(() => group.nodes.map((node) => node.id), [group.nodes])
   const selectableNodes = useMemo(() => {
     const selectedNodeIDs = group.nodes.map((node) => node.id)
 
     return nodes.filter((node) => !selectedNodeIDs.includes(node.id))
   }, [group.nodes, nodes])
-  const [selectedNodesToRemove, setSelectedNodesToRemove] = useState<string[]>([])
-  const [selectedNodesToAdd, setSelectedNodesToAdd] = useState<string[]>([])
-  const allNodes = useMemo(() => group.nodes.map((node) => node.id), [group.nodes])
   const groupAddNodesMutation = useGroupAddNodesMutation()
   const groupDelNodesMutation = useGroupDelNodesMutation()
 
-  const nodeTableColumns: {
-    key: string
-    name: string
-  }[] = [
-    { key: 'name', name: t('primitives.name') },
-    { key: 'protocol', name: t('primitives.protocol') },
-    { key: 'subscriptionID', name: t('primitives.subscription') },
-    { key: 'remove', name: t('actions.remove') }
-  ]
-
-  const renderNodeCell = useCallback(
-    (node: Group['nodes'][number], columnKey: Key) => {
-      const cellValue = node[columnKey as keyof Group['nodes'][number]] as ReactNode
-
-      switch (columnKey) {
-        case 'name':
-          return node.tag || node.name
-
-        case 'remove':
-          return (
-            <Button
-              color="danger"
-              isIconOnly
-              onPress={async () => {
-                await groupDelNodesMutation.mutateAsync({
-                  id: group.id,
-                  nodeIDs: [node.id]
-                })
-                await refetch()
-              }}
-            >
-              <IconTrash />
-            </Button>
-          )
-
-        default:
-          return cellValue
-      }
-    },
-    [group.id, groupDelNodesMutation, refetch]
-  )
-
   return (
-    <div className="flex flex-col gap-2">
-      <Accordion selectionMode="multiple" variant="splitted">
-        <AccordionItem key="subscriptions" title={`${t('primitives.subscription')} (${group.subscriptions.length})`}>
-          <Table
-            aria-label="subscription table"
-            selectionMode="multiple"
-            selectedKeys={selectedSubscriptionsToRemove}
-            onSelectionChange={(selectedSubscriptions) =>
-              setSelectedSubscriptionsToRemove(
-                selectedSubscriptions === 'all' ? allSubscriptions : Array.from(selectedSubscriptions as Set<string>)
+    <div className="grid grid-cols-1 gap-4 py-4 sm:grid-cols-2">
+      <Select
+        variant="bordered"
+        label={t('primitives.subscription')}
+        placeholder={t('primitives.subscription')}
+        isMultiline={true}
+        selectionMode="multiple"
+        labelPlacement="outside"
+        classNames={{ trigger: 'py-2' }}
+        items={subscriptions}
+        renderValue={(items) => (
+          <div className="flex flex-wrap gap-2">
+            {items.map((item) => (
+              <Chip
+                key={item.key}
+                classNames={{
+                  content: 'block truncate'
+                }}
+                onClose={() => groupDelSubscriptionsMutation.mutate({ id: group.id, subscriptionIDs: [item.data!.id] })}
+              >
+                {item.data!.tag}
+              </Chip>
+            ))}
+          </div>
+        )}
+      >
+        {(subscription) => (
+          <SelectItem key={subscription.id} textValue={subscription.id}>
+            {subscription.tag}
+          </SelectItem>
+        )}
+      </Select>
+
+      <Select
+        label={t('primitives.node')}
+        placeholder={t('primitives.node')}
+        variant="bordered"
+        isMultiline
+        selectionMode="multiple"
+        labelPlacement="outside"
+        classNames={{ trigger: 'py-2' }}
+        items={nodes
+          .map(({ id, tag, name }) => ({ id, name: tag || name, subscription: '' }))
+          .concat(
+            subscriptions
+              .map((subscription) =>
+                subscription.nodes.edges.map(({ id, tag, name }) => ({
+                  id,
+                  name: tag || name,
+                  subscription: subscription.tag || ''
+                }))
               )
-            }
-            topContent={
-              <div className="flex gap-2">
-                <Popover
-                  isOpen={isAddSubscriptionsTooltipOpen}
-                  onOpenChange={onAddSubscriptionsTooltipOpenChange}
-                  placement="left"
-                  showArrow
+              .flatMap((node) => node)
+          )}
+        renderValue={(items) => (
+          <div className="flex flex-wrap gap-2">
+            {items.map((item) => {
+              const nodeID = item.textValue!
+              const nodeName = item.data?.name
+
+              return (
+                <Chip
+                  key={item.key}
+                  classNames={{ base: 'max-w-full', content: 'truncate' }}
+                  onClose={() => groupDelNodesMutation.mutate({ id: group.id, nodeIDs: [nodeID] })}
                 >
-                  <PopoverTrigger>
-                    <Button
-                      color="primary"
-                      onPress={onAddSubscriptionsTooltipOpen}
-                      isDisabled={selectableSubscriptions.length === 0}
-                      isIconOnly
-                    >
-                      <IconPlus />
-                    </Button>
-                  </PopoverTrigger>
-
-                  <PopoverContent>
-                    <div className="flex flex-col gap-4 p-4">
-                      <Listbox
-                        aria-label="select subscriptions"
-                        variant="flat"
-                        selectionMode="multiple"
-                        selectedKeys={selectedSubscriptionsToAdd}
-                        onSelectionChange={(selectedSubscriptions) =>
-                          setSelectedSubscriptionsToAdd(
-                            selectedSubscriptions === 'all'
-                              ? allSubscriptions
-                              : Array.from(selectedSubscriptions as Set<string>)
-                          )
-                        }
-                      >
-                        {selectableSubscriptions.map((subscription) => (
-                          <ListboxItem key={subscription.id}>{subscription.tag}</ListboxItem>
-                        ))}
-                      </Listbox>
-
-                      <Button
-                        color="primary"
-                        size="sm"
-                        isDisabled={selectedSubscriptionsToAdd.length === 0}
-                        onPress={async () => {
-                          await groupAddSubscriptionsMutation.mutateAsync({
-                            id: group.id,
-                            subscriptionIDs: selectedSubscriptionsToAdd
-                          })
-
-                          await refetch()
-
-                          setSelectedSubscriptionsToAdd([])
-                          onAddSubscriptionsTooltipClose()
-                        }}
-                      >
-                        {t('actions.confirm')}
-                      </Button>
-                    </div>
-                  </PopoverContent>
-                </Popover>
-                <Button
-                  color="danger"
-                  onPress={async () => {
-                    await groupDelSubscriptionsMutation.mutateAsync({
-                      id: group.id,
-                      subscriptionIDs: selectedSubscriptionsToRemove
-                    })
-
-                    await refetch()
-
-                    setSelectedSubscriptionsToRemove([])
-                  }}
-                  isDisabled={selectedSubscriptionsToRemove.length === 0}
-                  isIconOnly
-                >
-                  <IconTrash />
-                </Button>
-              </div>
-            }
-          >
-            <TableHeader columns={subscriptionTableColumns}>
-              {(column) => <TableColumn key={column.key}>{column.name}</TableColumn>}
-            </TableHeader>
-
-            <TableBody items={group.subscriptions}>
-              {(item) => (
-                <TableRow key={item.id}>
-                  {(columnKey) => <TableCell>{renderSubscriptionCell(item, columnKey)}</TableCell>}
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </AccordionItem>
-
-        <AccordionItem key="nodes" title={`${t('primitives.node')} (${nodes.length})`}>
-          <Table
-            aria-label="node table"
-            selectionMode="multiple"
-            selectedKeys={selectedNodesToRemove}
-            onSelectionChange={(selectedNodes) =>
-              setSelectedNodesToRemove(selectedNodes === 'all' ? allNodes : Array.from(selectedNodes as Set<string>))
-            }
-            topContent={
-              <div className="flex gap-2">
-                <Popover
-                  isOpen={isAddNodesTooltipOpen}
-                  onOpenChange={onAddNodesTooltipOpenChange}
-                  placement="left"
-                  showArrow
-                >
-                  <PopoverTrigger>
-                    <Button
-                      color="primary"
-                      onPress={onAddNodesTooltipOpen}
-                      isDisabled={selectableNodes.length === 0}
-                      isIconOnly
-                    >
-                      <IconPlus />
-                    </Button>
-                  </PopoverTrigger>
-
-                  <PopoverContent>
-                    <div className="flex flex-col gap-4 p-4">
-                      <Listbox
-                        aria-label="select subscriptions"
-                        variant="flat"
-                        selectionMode="multiple"
-                        selectedKeys={selectedNodesToAdd}
-                        onSelectionChange={(selectedNodes) =>
-                          setSelectedNodesToAdd(
-                            selectedNodes === 'all' ? allNodes : Array.from(selectedNodes as Set<string>)
-                          )
-                        }
-                      >
-                        {selectableNodes.map((subscription) => (
-                          <ListboxItem key={subscription.id}>{subscription.tag}</ListboxItem>
-                        ))}
-                      </Listbox>
-
-                      <Button
-                        color="primary"
-                        size="sm"
-                        isDisabled={selectedNodesToAdd.length === 0}
-                        onPress={async () => {
-                          await groupAddNodesMutation.mutateAsync({
-                            id: group.id,
-                            nodeIDs: selectedNodesToAdd
-                          })
-
-                          await refetch()
-
-                          setSelectedNodesToAdd([])
-                          onAddNodesTooltipClose()
-                        }}
-                      >
-                        {t('actions.confirm')}
-                      </Button>
-                    </div>
-                  </PopoverContent>
-                </Popover>
-                <Button
-                  color="danger"
-                  onPress={async () => {
-                    await groupDelNodesMutation.mutateAsync({
-                      id: group.id,
-                      nodeIDs: selectedNodesToRemove
-                    })
-
-                    await refetch()
-
-                    setSelectedNodesToRemove([])
-                  }}
-                  isDisabled={selectedNodesToRemove.length === 0}
-                  isIconOnly
-                >
-                  <IconTrash />
-                </Button>
-              </div>
-            }
-          >
-            <TableHeader columns={nodeTableColumns}>
-              {(column) => <TableColumn key={column.key}>{column.name}</TableColumn>}
-            </TableHeader>
-
-            <TableBody emptyContent={'No rows to display.'} items={group.nodes}>
-              {(item) => (
-                <TableRow key={item.id}>
-                  {(columnKey) => <TableCell>{renderNodeCell(item, columnKey)}</TableCell>}
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </AccordionItem>
-      </Accordion>
+                  {nodeName} {item.data?.subscription && `(${item.data.subscription})`}
+                </Chip>
+              )
+            })}
+          </div>
+        )}
+      >
+        {(node) => (
+          <SelectItem key={node.id} textValue={node.id}>
+            {node.name} {node.subscription && `(${node.subscription})`}
+          </SelectItem>
+        )}
+      </Select>
     </div>
   )
 }
