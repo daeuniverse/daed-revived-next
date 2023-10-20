0f0903eb987616d2a52333835f1c58b1b3958312
kunish <kunish.butt@gmail.com>
2023-10-21 05:50:59 +0800

    feat: drop lottie

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { DotLottiePlayer } from '@dotlottie/react-player'
 import RiveComponent from '@rive-app/react-canvas'
 import { useQuery } from '@tanstack/react-query'
 import { gql } from 'graphql-request'
@@ -36,12 +35,6 @@ this is a test message
   return (
     <div className="flex flex-col gap-4">
       <div className="flex items-center justify-center gap-2">
-        <DotLottiePlayer className="h-80 w-80" src="/animation_lny15oo1.lottie" autoplay loop />
-        <DotLottiePlayer className="h-80 w-80" src="/animation_lny18m5n.lottie" autoplay loop />
-        <DotLottiePlayer className="h-80 w-80" src="/animation_lny19oah.lottie" autoplay loop />
-        <DotLottiePlayer className="h-80 w-80" src="/animation_lny163dx.lottie" autoplay loop />
-        <DotLottiePlayer className="h-80 w-80" src="/animation_lny1861i.lottie" autoplay loop />
-
         <RiveComponent src="/3890-8146-moon-scan.riv" className="h-80 w-80" />
         <RiveComponent src="/569-6666-blue-planet.riv" className="h-80 w-80" />
       </div>
