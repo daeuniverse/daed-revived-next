27a9ec68d66e72c2b6e8c245caca09df257c948f
kunish <kunish.butt@gmail.com>
2023-10-25 21:33:32 +0800

    feat(rule): update layout

--- src/app/(protected)/network/page.tsx
+++ src/app/(protected)/network/page.tsx
@@ -21,7 +21,7 @@ export default function NetworkPage() {
           subscriptions={subscriptionsQuery.data?.subscriptions || []}
         />
 
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+        <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
           <NodeSection nodes={nodesQuery.data?.nodes.edges || []} isLoading={nodesQuery.isLoading} />
 
           <SubscriptionSection subscriptions={subscriptionsQuery.data?.subscriptions || []} />
--- src/components/ResourceRadioGroup.tsx
+++ src/components/ResourceRadioGroup.tsx
@@ -6,7 +6,7 @@ export const ResourceRadio = forwardRef<ElementRef<typeof Radio>, RadioProps>((p
     <Radio
       ref={ref}
       classNames={{
-        base: 'm-0 gap-4 rounded-lg border-2 border-transparent bg-content1 p-4 hover:bg-content2 data-[selected=true]:border-primary',
+        base: 'm-0 max-w-full gap-4 rounded-lg border-2 border-transparent bg-content1 p-4 hover:bg-content2 data-[selected=true]:border-primary',
         labelWrapper: 'gap-2',
         label: 'inline-flex items-center gap-2'
       }}
@@ -17,12 +17,6 @@ export const ResourceRadio = forwardRef<ElementRef<typeof Radio>, RadioProps>((p
 ResourceRadio.displayName = 'ResourceRadio'
 
 export const ResourceRadioGroup = forwardRef<ElementRef<typeof RadioGroup>, RadioGroupProps>((props, ref) => (
-  <RadioGroup
-    ref={ref}
-    classNames={{
-      wrapper: 'flex flex-row gap-4'
-    }}
-    {...props}
-  />
+  <RadioGroup ref={ref} classNames={{ wrapper: 'gap-4' }} {...props} />
 ))
 ResourceRadioGroup.displayName = 'ResourceRadioGroup'
